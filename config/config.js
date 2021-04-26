63411627fcb5978b6fc7278b698ef3379a43fed9
urja <urjasrivastava123@gmail.com>
2021-04-26 22:54:08 +0530

    Procfile

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const config = {
-    env: process.env.NODE_ENV || 'development',
+    env: process.env.NODE_ENV || 'production',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: "mongodb+srv://mern-user:hhhbc2bvg983YlOl@montage.dgaqp.mongodb.net/Montage?retryWrites=true&w=majority",
-    serverUrl: process.env.serverUrl || 'http://localhost:3000'
+    serverUrl: process.env.serverUrl || 'https://montage-explore.herokuapp.com/'
 }
     export default config
